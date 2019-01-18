06a53faf9e94ec289966bf0e51cdb0eeaf9eb211
Mikolla <miqolay@gmail.com>
2019-01-18 18:45:00 +0500

    resttemplate add

--- src/main/java/ru/springbootrest/controller/HomeController.java
+++ src/main/java/ru/springbootrest/controller/HomeController.java
@@ -7,12 +7,16 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class HomeController {
 
+	RestTemplate restTemplate = new RestTemplate();
+	static final String URL_USERS = "http://localhost:8080/rest/user";
 
 /*	@RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
 	public String getWelcome(ModelMap modelMap) {
@@ -33,8 +37,23 @@ public class HomeController {
 		return "adminrest";
 	}
 
-	@RequestMapping(value = "/userrest")
-	public String userPageREST() {
+	/*
+	@RequestMapping(value = "/admin", method = RequestMethod.GET)
+	public String adminPage(ModelMap modelMap) throws Exception {
+		modelMap.addAttribute("adminRole", new Role("Admin"));
+		modelMap.addAttribute("userRole", new Role("User"));
+		modelMap.addAttribute("adminName", getPrincipal());
+		modelMap.addAttribute("users", userService.getAllUsers());
+		return "admin";
+	}
+	 */
+
+	@RequestMapping(value = "/userrest", method = RequestMethod.GET)
+	public String userPageREST(Model modelMap) {
+		String jsonUsersString = restTemplate.getForObject(URL_USERS + "/all", String.class);
+		System.out.println(jsonUsersString);
+		modelMap.addAttribute("jsonusers", jsonUsersString);
+
 		return "userrest";
 	}
 
