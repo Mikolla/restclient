b3c34aa92b24bdfccf628129cf19e7a6e06635db
Mikolla <miqolay@gmail.com>
2019-01-21 19:01:40 +0500

    resttemplate add edituser w/o ajax

--- src/main/java/ru/springbootrest/controller/HomeController.java
+++ src/main/java/ru/springbootrest/controller/HomeController.java
@@ -2,11 +2,15 @@ package ru.springbootrest.controller;
 
 
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
@@ -29,7 +33,7 @@ public class HomeController {
 
 	@RequestMapping(value = "/login")
 	public String loginPage() {
-	    return "login";
+		return "login";
 	}
 
 	@RequestMapping("/adminrest")
@@ -51,11 +55,46 @@ public class HomeController {
 	@RequestMapping(value = "/userrest", method = RequestMethod.GET)
 	public String userPageREST(Model modelMap) {
 		String jsonUsersString = restTemplate.getForObject(URL_USERS + "/all", String.class);
-		System.out.println(jsonUsersString);
 		modelMap.addAttribute("jsonusers", jsonUsersString);
-
 		return "userrest";
 	}
+
+
+	@RequestMapping(value = "/userrest/add", method = RequestMethod.POST)
+	public String addUserPage(@RequestParam(value = "name") String name,
+							  @RequestParam(value = "login") String login,
+							  @RequestParam(value = "password") String password,
+							  @RequestParam(value = "role") String roleName) {
+		//       System.out.println("name = " +  name + " login = " + login + " password = " + password + " rolename = " + roleName);
+		int roleId = roleName.equals("Admin") ? 1 : 2;
+		String jsonNewUserString = "{\"name\":\"" + name + "\",\"login\":\"" + login + "\",\"password\":\"" + password + "\",\"roles\":[{\"id\":" + roleId + ",\"roleName\":\"" + roleName + "\"}]}";
+		//	String sss = "{\"name\":\"66qa\",\"login\":\"66qa\",\"password\":\"66qa\",\"roles\":[{\"id\":2,\"roleName\":\"User\"}]}";
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		// Data attached to the request.
+		HttpEntity<String> requestBody = new HttpEntity<>(jsonNewUserString, headers);
+		String e = restTemplate.postForObject(URL_USERS + "/add", requestBody, String.class);
+
+//String addedUserString = restTemplate.
+		return "redirect:/userrest";
+	}
+
+
+	@RequestMapping(value = { "/admin/edit" }, method = RequestMethod.POST)
+	public String updateUser(@RequestParam(value = "id") String id,
+							 @RequestParam(value = "login") String login,
+							 @RequestParam(value = "name") String name,
+							 @RequestParam(value = "password") String password,
+							 @RequestParam(value = "role") String roleName) {
+
+		return "redirect:/admin";
+	}
+
+
+
 
 	@RequestMapping(value = "/access_denied")
 	public String deniesPage() {
--- src/main/resources/static/js/userdatafill.js
+++ src/main/resources/static/js/userdatafill.js
@@ -1,29 +1,46 @@
 $(document).ready(function() {
 
 
-    var html = $('#jsonUsersString').text();
-    console.log(html);
+    var userListStr = $('#jsonUsersString').text();
+    var userList = jQuery.parseJSON( userListStr );
+    console.log(userList);
+    console.log("id = " + userList[0].id);
+    console.log("name = " + userList[0].name);
+    console.log("login = " + userList[0].login);
+    console.log("password = " + userList[0].password);
+    console.log("role = " + userList[0].roles[0].roleName);
+
+
+    var myform = document.getElementById("jsonUsersString").innerHTML;
+    console.log(myform);
 
 
 
 
-    $.ajax({
-        url: "http://localhost:8080/rest/user/principal"
-    }).then(function(data) {
-        var userlogined = data;
-      console.log(userlogined.id);
-      console.log(userlogined.name);
-      console.log(userlogined.login);
-      console.log(userlogined.password);
-      console.log(userlogined.roles[0].roleName);
 
-        $('#hellouser').append( 'Hello ' + userlogined.name + '!' );
-        $('#userdataid').append( 'Authorised user data: ');
-        $('#userdataid').append( 'id = ' + userlogined.id + '; ');
-        $('#userdatalogin').append( 'login = ' + userlogined.login  + '; ');
-        $('#userdataname').append( 'name = ' + userlogined.name  + '; ');
-        $('#userdatapassword').append( 'password = ' + userlogined.password  + '; ');
-        $('#userdatarole').append( 'role = ' + userlogined.roles[0].roleName  + '.');
+    for(var i = 0; i < userList.length; i++) {
+        var role = userList[i].roles[0];
+        console.log(userList[i].id + ' ' + userList[i].login + ' ' + userList[i].name + ' ' + userList[i].password + ' ' + role.roleName + '\r\n');
 
-    });
+        $('#listrole').append(
+            '<tr>' +
+            '<td>' + userList[i].id + '</td>' +
+            '<td>' + userList[i].login + '</td>' +
+            '<td>' + userList[i].name + '</td>' +
+            '<td>' + userList[i].password + '</td>' +
+            '<td>' + role.roleName + '</td>' +
+            '<td>' +
+            '<button class="btn btn-success" id="edButton" data-toggle="modal" data-target="#myModal">Edit</button>' +
+            ' ' +
+            '<button type="button" class="btn btn-primary" id="delButton">Delete</button>'
+            + '</td>'
+            + '</tr>'
+        );
+
+    }
+
+
+
+
+
 });
